d129b54850251fed6378cb1590cf92456d734d13
YHC <moyunyongan@gmail.com>
2025-09-05 14:02:16 -0700

    fix and enable sphinx latex math support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ release = "0.0.1"
 extensions = [
     "myst_parser",  # Markdown support
     "sphinxcontrib.mermaid",  # Mermaid diagrams support
+    "sphinx.ext.mathjax",  # Latex math support
 ]
 
 templates_path = ["_templates"]
