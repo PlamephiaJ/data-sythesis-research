1e314034bf74dff4ac19d23a6a82f8158b8807ed
YHC <yu.hao.c@outlook.com>
2026-02-10 13:23:58 -0800

    add imbalance ratio

--- src/detection_model/train.sh
+++ src/detection_model/train.sh
@@ -1,3 +1,3 @@
 source .venv/bin/activate
 
-python train.py -m data.train_ratio=0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0
+python src/detection_model/train.py -m data.train_ratio=0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0
